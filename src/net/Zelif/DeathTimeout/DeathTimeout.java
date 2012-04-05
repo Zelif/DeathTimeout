fa3d1943e96bbeb71ab7c1a9624b2200208a6f1a
Zelif <jarodk.1988@gmail.com>
2012-04-05 15:12:22 +1000

    Adds 2 new commands fixes edit bug

--- src/net/Zelif/DeathTimeout/DeathTimeout.java
+++ src/net/Zelif/DeathTimeout/DeathTimeout.java
@@ -1,6 +1,12 @@
 package net.Zelif.DeathTimeout;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -28,6 +34,7 @@ public class DeathTimeout extends JavaPlugin
 	{
 		reloadConfig();
 	}
+	@SuppressWarnings("rawtypes")
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
 	{
 		if(commandLabel.equalsIgnoreCase("dt-reload") && sender.hasPermission("DeathTimeout.reload"))
@@ -38,13 +45,43 @@ public class DeathTimeout extends JavaPlugin
 		}
 		else if(commandLabel.equalsIgnoreCase("dt-edit") && sender.hasPermission("DeathTimeout.edit"))
 		{   
-			if(!(args[1]==null))
+			if(args.length > 0)
 			{   //ingame command to edit config, can also be used in console
-				int changeTime = Integer.parseInt(args[1]);
-				this.getConfig().addDefault("SecondsBanned",changeTime);
-				sender.sendMessage("Changed ban time to "+(changeTime/1000)/60+"mins "+(changeTime/1000)% 60+"sec");
+				int changeTime = Integer.parseInt(args[0]);
+				this.getConfig().set("SecondsBanned",changeTime);
+				sender.sendMessage("Changed ban time to "+changeTime/60+"mins "+changeTime% 60+"sec");
 				saveConfig();
 			return true;
+			}
+		}
+		else if(commandLabel.equalsIgnoreCase("dt-unban") && sender.hasPermission("DeathTimeout.unban"))
+		{   
+			if(args.length > 0)
+			{   //ingame command to unban players, can also be used in console
+				OfflinePlayer targetPlayer = Bukkit.getServer().getOfflinePlayer(args[0]);
+				if(targetPlayer.hasPlayedBefore() && DeathTListener.BanID.get(args[0].toLowerCase()) != null)
+				{
+					DeathTListener.BanID.remove(args[0].toLowerCase());
+					sender.sendMessage("Removed " + args[0] + " from banlist.");
+					return true;
+				}
+				sender.sendMessage(args[0] + " is either not on the banlist or has not played on the server.");
+				return false;
+			}
+			sender.sendMessage("Needs a valid player name.");
+		}
+		else if(commandLabel.equalsIgnoreCase("dt-list") && sender.hasPermission("DeathTimeout.list"))
+		{
+			Set banList = DeathTListener.BanID.entrySet();
+			Iterator i = banList.iterator();
+			// Display elements
+			while(i.hasNext()){
+				Map.Entry me = (Map.Entry)i.next();
+				long timz = (Long) me.getValue();
+				long now = System.currentTimeMillis();
+				long diff = timz - now;
+				sender.sendMessage(me.getKey() + " is banned for: " + (diff/1000)/60 + "min " + (diff/1000)%60 + "sec.");
+				return true;
 			}
 		}
 		return false;
