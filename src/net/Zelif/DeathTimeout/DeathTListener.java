30dfff78b4afd455d1aa7696c5488fd497857880
Zelif <jarodk.1988@gmail.com>
2012-07-20 00:42:04 +1000

    Update src/net/Zelif/DeathTimeout/DeathTListener.java

--- src/net/Zelif/DeathTimeout/DeathTListener.java
+++ src/net/Zelif/DeathTimeout/DeathTListener.java
@@ -1,6 +1,4 @@
-package net.Zelif.DeathTimeout;
-
-import java.util.HashMap;
+package net.zelif.DeathTimeout;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.EventHandler;
@@ -11,71 +9,41 @@ import org.bukkit.event.player.PlayerLoginEvent.Result;
 
 public class DeathTListener implements Listener
 {
-	public static HashMap<String, Long> BanID= new HashMap<String, Long>();
 	DeathTimeout plugin;
 	public DeathTListener(DeathTimeout instance)
 	{
 		plugin = instance;
     }
-
-	public boolean checkBan(PlayerLoginEvent event , Player p )
+	
+	@EventHandler(priority = EventPriority.HIGH)
+	public void onPlayerLogin(PlayerLoginEvent event)
 	{
-		if(BanID.containsKey(p.getName().toLowerCase()))
-		{
-			long tempTime = BanID.get(p.getName().toLowerCase());
-			long now = System.currentTimeMillis();
-			long diff = tempTime - now;
-			if(diff <= 0)
-			{
-				return false;
-			}
-			else
-			{
-				String caltime = ((diff/1000)/60+"mins "+(diff/1000)% 60+"sec");
-				String timeleft = plugin.getConfig().getString("Kickrejoin").replace("time", caltime);
-				event.setKickMessage(timeleft);
-				return true;
-			}	
-		}
-		return false;
-	}
-			
-    @EventHandler(priority = EventPriority.HIGH)
-    public void onPlayerLogin(PlayerLoginEvent event)
-    {
-		Player p = event.getPlayer();
-    	if(BanID.get(p.getName().toLowerCase()) != null)
+		String pName = event.getPlayer().getName().toString();
+    	if(timeCal.checkName(pName) == true)  //Checks to see if name is in the hashmap on login
     	{
-    		if (checkBan(event, p)==true)
+    		if (timeCal.checkBan(event) == true)   //Checks to see if the player is banned and/or the time on a ban is up
     		{
-    			event.setResult(Result.KICK_OTHER);
+    			event.setResult(Result.KICK_OTHER); //Kicks if player is still banned
     		}
     		else
     		{
-    			BanID.remove(p.getName().toLowerCase());
+    			timeCal.removeBanID(pName); //Removes the name if the time has expired
     		}
     	}
 	}
-	
-    @EventHandler(priority = EventPriority.HIGH)
+
+
+	@EventHandler(priority = EventPriority.HIGH)
 	public void onEntityDeathEvent(EntityDeathEvent event)
-    {
+   	{
     	if (event.getEntity() instanceof Player)
-    	{
-    		Player pl = (Player)event.getEntity();
-    		if(!(pl.hasPermission("deathtimeout.bypass"))){ 										//bypass the ban if user has this node
-    			pl.getInventory().setHelmet(null);
-    			pl.getInventory().setChestplate(null);
-    			pl.getInventory().setLeggings(null);
-    			pl.getInventory().setBoots(null);
-    			pl.getInventory().clear();															//Removes all inventory items ;D
-    			int banTime = plugin.getConfig().getInt("SecondsBanned");							//Grabs the amount of seconds to be banned from config
-    			long tempT1 = System.currentTimeMillis()+(banTime*1000); 							//Finds the current time in milliseconds & adds bantime converted to millsecs 
-    			BanID.put(pl.getName().toLowerCase(),tempT1); 										//Uses a hashmap with the time from tempT1
-    			String timecal = (banTime/60+"mins "+banTime%60+"sec"); 							//Calculates the mins and seconds and sets to a string
-    			String timevar = plugin.getConfig().getString("Kickmsg").replace("time", timecal ); //Replaces the word time with the timecal variable
-    			pl.kickPlayer(timevar); 															//kicks player with the msg from timeval
-    		}
+    	{    		
+			Player p = (Player)event.getEntity();
+    		if(!(p.hasPermission("deathtimeout.bypass"))) 		//bypass the ban if user has this node
+			{ 								
+				timeCal.deathCheck(event);  //checks to see if player can be banned.
+			}
     	}
-    }
+   	}
+	
 }
