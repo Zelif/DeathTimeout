311dc4ec6108d581e237ffb93431c8ba7bf167ae
Zelif <jarodk.1988@gmail.com>
2012-07-20 00:45:41 +1000

    Slim down the Listener

--- src/net/Zelif/DeathTimeout/DeathTListener.java
+++ src/net/Zelif/DeathTimeout/DeathTListener.java
@@ -13,37 +13,37 @@ public class DeathTListener implements Listener
 	public DeathTListener(DeathTimeout instance)
 	{
 		plugin = instance;
-    }
+   	}
 	
 	@EventHandler(priority = EventPriority.HIGH)
 	public void onPlayerLogin(PlayerLoginEvent event)
 	{
 		String pName = event.getPlayer().getName().toString();
-    	if(timeCal.checkName(pName) == true)  //Checks to see if name is in the hashmap on login
-    	{
-    		if (timeCal.checkBan(event) == true)   //Checks to see if the player is banned and/or the time on a ban is up
+    		if(timeCal.checkName(pName) == true)  //Checks to see if name is in the hashmap on login
     		{
-    			event.setResult(Result.KICK_OTHER); //Kicks if player is still banned
+    			if (timeCal.checkBan(event) == true)   //Checks to see if the player is banned and/or the time on a ban is up
+    			{
+    				event.setResult(Result.KICK_OTHER); //Kicks if player is still banned
+    			}
+    			else
+    			{
+    				timeCal.removeBanID(pName); //Removes the name if the time has expired
+    			}
     		}
-    		else
-    		{
-    			timeCal.removeBanID(pName); //Removes the name if the time has expired
-    		}
-    	}
 	}
 
 
 	@EventHandler(priority = EventPriority.HIGH)
 	public void onEntityDeathEvent(EntityDeathEvent event)
    	{
-    	if (event.getEntity() instanceof Player)  //only gathers a players death
-    	{    		
+    		if (event.getEntity() instanceof Player)  //only gathers a players death
+    		{    		
 			Player p = (Player)event.getEntity();
-    		if(!(p.hasPermission("deathtimeout.bypass"))) 		//bypass the ban if user has this node
+    			if(!(p.hasPermission("deathtimeout.bypass"))) 		//bypass the ban if user has this node
 			{ 								
 				timeCal.deathCheck(event);  //checks to see if player can be banned.
 			}
-    	}
+    		}
    	}
 	
 }
